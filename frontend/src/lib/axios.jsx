4cb26c31e4b41738a4599a5db80d6f42a443445e
anubhavraiii <realar99@gmail.com>
2025-08-09 01:09:26 +0530

    OAuth update

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	//baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: API_URL,
 	withCredentials: true, // send cookies to the server
 });
 
