44d3bc640be46bfc5449e6c26686a2713e6768c1
anubhavraiii <realar99@gmail.com>
2025-08-08 21:45:05 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import passport from './lib/passport.js';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 
@@ -14,6 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL, // Allow requests from your frontend URL
+    credentials: true // Allow cookies to be sent
+}));
 
 app.use(express.json({ limit: "10mb" })); // allow express to parse JSON data
 app.use(cookieParser()); // to parse cookies from the request
